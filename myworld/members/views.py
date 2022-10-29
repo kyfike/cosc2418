af86d213abf7ac3df715eccc7b3c32614dd67230
Ky Fike <kyfike@gmail.com>
2022-10-29 15:22:12 -0600

    addrecord functionality

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Members
 
 def index(request):
@@ -9,3 +10,14 @@ def index(request):
     'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Members(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
