4089671a35dd4505eb521d8c51b95476c2f23152
Ky Fike <kyfike@gmail.com>
2022-10-29 15:00:36 -0600

    add 5 entries in SQL table

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  mymembers = Members.objects.all().values()
+  output = ""
+  for x in mymembers:
+    output += x["firstname"]
+  return HttpResponse(output)
