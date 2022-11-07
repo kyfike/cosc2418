dc36115e196876f9a9687d428ea2027ec3326faa
Ky Fike <kyfike@gmail.com>
2022-11-06 21:03:25 -0700

    app, template, and static page (css)

--- hello_django/hello/views.py
+++ hello_django/hello/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import HttpResponse
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+
+
+print("http://127.0.0.1:8000/hello/VSCode")
