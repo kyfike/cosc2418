794febb52388303a76fab2ee421ffdf0f4caba46
Ky Fike <kyfike@gmail.com>
2022-11-06 21:37:06 -0700

    boilerplate snippet

--- hello_django/hello/urls.py
+++ hello_django/hello/urls.py
@@ -2,6 +2,9 @@ from django.urls import URLPattern, path
 from hello import views
 
 urlpatterns = [
+    path("services/", views.services, name="services"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("hello/<name>", views.hello_there, name="hello_there"),
     path("", views.home, name="home")
 ]
--- hello_django/hello/views.py
+++ hello_django/hello/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def services(request):
+    return render(request, "hello/services.html")
 
 def hello_there(request, name):
     return render(
